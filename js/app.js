d44902a0fbcec424b97fdd3664a268898e751b55
cheppimdq <cheppimdp@gmail.com>
2023-12-24 17:47:46 -0300

    .·.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 // SCROLL
 window.addEventListener('load', function () {
     var path = window.location.pathname;
-    if (path !== '/' && path.indexOf('/index.html') === -1) {
-        window.scrollTo(0, window.innerHeight * 0.91); // 91vh
+    function calculateScrollOffset() {
+        if (window.innerWidth <= 767) {
+            return window.innerHeight * 1.0; // 100vh
+        } else {
+            return window.innerHeight * 0.91; // 91vh para pantallas mayores a 767px
         }
     }
-);
+    if (path !== '/' && path.indexOf('/index.html') === -1) {
+        window.scrollTo(0, calculateScrollOffset());
+    }
+});
